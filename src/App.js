ee9dd32180bb3bfa8245f415092f53910e1763c4
evondev <trananhtuan400@gmail.com>
2022-01-17 22:21:08 +0700

    Upload base template react course

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./App.scss";
+import React from "react";
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "primary-gradient": `linear-gradient(
+          86.88deg,
+          #7d6aff 1.38%,
+          #ffb86c 64.35%,
+          #fc2872 119.91%
+        )`,
+        "secondary-gradient": `linear-gradient(86.88deg, #20e3b2, #2cccff)`,
+      },
+    },
+  },
+  plugins: [],
+};
